6b476861174a367801ae78f7bf59583ac14de549
whereismidel <zhariuk.artur@gmail.com>
2023-08-23 01:47:47 +0300

    - init project

---
+++ src/main/java/dev/midel/storeanalyzingbot/StoreAnalyzingBotApplication.java
@@ -0,0 +1,13 @@
+package dev.midel.storeanalyzingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreAnalyzingBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreAnalyzingBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/midel/storeanalyzingbot/StoreAnalyzingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.midel.storeanalyzingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreAnalyzingBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
